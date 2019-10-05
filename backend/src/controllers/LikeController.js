297ad12ee5009783ea113a55f44c066c1a956f52
James Amorim <jamesfsamorim@gmail.com>
2019-10-04 22:15:46 -0300

    gitflow-feature-stash: day_4
    Finished app with socket io

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -12,7 +12,16 @@ module.exports = {
             res.status(400).json({error: "Dev not exists"})
         }
         if(targetDev.likes.includes(loggedDev._id)) {
-            console.log("DEU MATCH!")
+            const loggedSocket = req.connectedUsers[user]
+            const targetSocket = req.connectedUsers[devId]
+
+            if(loggedSocket) {
+                req.io.to(loggedSocket).emit('match', targetDev)
+            }
+
+            if(targetSocket) {
+                req.io.to(targetSocket).emit('match', loggedDev)
+            }
         }
         loggedDev.likes.push(targetDev._id)
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,31 @@
 const express = require('express')
-const server = express()
+const app = express()
 const routes = require('./routes')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const server = require('http').Server(app)
+const io = require("socket.io")(server)
+
+const connectedUsers = {}
+io.on('connection', socket => {
+    const { user } = socket.handshake.query
+    console.log("user: ", user)
+
+    connectedUsers[user] = socket.id
+})
 
 mongoose.connect("mongodb+srv://omni:omniadmin@tindevcluster-mt9dj.mongodb.net/tindev_db?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 
-server.use(cors())
-server.use(express.json())
-server.use(routes)
+app.use((req, res, next) => {
+    req.io = io
+    req.connectedUsers = connectedUsers
+
+    next()
+})
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+
 server.listen('3333')
